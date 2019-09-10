8d922f75cc3895e296fe812fa3c3111f1a1301c6
Sumit Kumar <sumit.kumar04@nagarro.com>
2019-09-10 12:53:58 +0530

    Java-8 changes explained with examples

--- Java8Examples/src/com/sumit/playjava/MainClass.java
+++ Java8Examples/src/com/sumit/playjava/MainClass.java
@@ -1,5 +1,10 @@
 package com.sumit.playjava;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
 import com.sumit.playjava.defaultMethod.ImplementInterfaceWithDefaultMethod;
 import com.sumit.playjava.staticMethod.InterfaceWithStaticMethod;
 
@@ -15,6 +20,48 @@ public class MainClass {
 		 * No need to create object of class implementing it.
 		 */
 		InterfaceWithStaticMethod.getStaticData();
+		
+		/*
+		 * Functional Interfaces are as follows:
+		 */
+		
+		//1.Predicate: return boolean value
+		Predicate<String> pred=new Predicate<String>() {
+			@Override
+			public boolean test(String str) {
+				return str.length()>10;
+			}
+		};
+		
+		String s="PlayJava !!!";
+		System.out.println("Boolean value from predicate: "+pred.test(s));
+		
+		//2.Consumer: this accepts a single argument and returns no result.
+		Consumer<Integer> consumer=new Consumer<Integer>() {
+			@Override
+			public void accept(Integer i) {
+				System.out.println("Nothing is returned in case of consumer");
+			}
+		};
+		consumer.accept(10);
+		
+		//3.Function: this interface accepts one argument and return one result.
+		Function<Integer,String> function=new Function<Integer,String>() {
+			@Override
+			public String apply(Integer t) {
+				return "The data is: "+t;
+			}
+		};
+		System.out.println(function.apply(100));
+		
+		//4.Supplier: this accepts nothing but return a value.
+		Supplier<String> supplier=new Supplier<String>() {
+			@Override
+			public String get() {
+				return "Data from supplier functional interface: "+Math.random();
+			}
+		};
+		System.out.println(supplier.get());
 	}
 
 }
